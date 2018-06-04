f8fc65182c463e0755903dc700208a75f30b9152
yaoqijun <yaoqijunmail@gmail.com>
2018-06-04 23:35:41 +0800

    add all

--- src/main/java/org/yqj/dao/demo/config/DB1Config.java
+++ src/main/java/org/yqj/dao/demo/config/DB1Config.java
@@ -1,6 +1,5 @@
 package org.yqj.dao.demo.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -44,23 +43,23 @@ public class DB1Config {
         return dataSource;
     }
 
-    private DataSource createDruidDataSource(){
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/db1?autoReconnect=true&useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("anywhere");
-        dataSource.setMinIdle(10);
-        dataSource.setMaxActive(50);
-        dataSource.setMaxWait(30000);
-        dataSource.setInitialSize(10);
-        dataSource.setRemoveAbandoned(true);
-        dataSource.setRemoveAbandonedTimeout(280); // sec
-        dataSource.setTimeBetweenEvictionRunsMillis(60000);
-        dataSource.setMinEvictableIdleTimeMillis(300000); //
-        dataSource.setValidationQuery("SELECT 1 FROM DUAL");
-        dataSource.setTestOnBorrow(true);
-        return dataSource;
-    }
+//    private DataSource createDruidDataSource(){
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/db1?autoReconnect=true&useSSL=false");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("anywhere");
+//        dataSource.setMinIdle(10);
+//        dataSource.setMaxActive(50);
+//        dataSource.setMaxWait(30000);
+//        dataSource.setInitialSize(10);
+//        dataSource.setRemoveAbandoned(true);
+//        dataSource.setRemoveAbandonedTimeout(280); // sec
+//        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+//        dataSource.setMinEvictableIdleTimeMillis(300000); //
+//        dataSource.setValidationQuery("SELECT 1 FROM DUAL");
+//        dataSource.setTestOnBorrow(true);
+//        return dataSource;
+//    }
 
 }
